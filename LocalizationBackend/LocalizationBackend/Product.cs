828ae5d53f30a9068e0ff6a657b0fbad9808dcfd
Enes Tunahan <enesstunahan@gmail.com>
2024-07-01 12:51:44 +0300

    ProductsController and products class has been created

---
+++ LocalizationBackend/LocalizationBackend/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalizationBackend
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string NameEn { get; set; }
+        public string NameTr { get; set; }
+    }
+}

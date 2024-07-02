3ada98c28417a8b351c1b71aebd347eb7bcf9212
Enes Tunahan <enesstunahan@gmail.com>
2024-07-02 21:27:38 +0300

    dil değişikliği esnasında veriler tekrar çekileceği için reac-loader-spinner kütüphenesi kullanılarak loading eklendi

--- LocalizationFronted/src/Components/Data/Data.jsx
+++ LocalizationFronted/src/Components/Data/Data.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './Data.css'
+import { Oval } from 'react-loader-spinner';
 
 const DataComponent = ({ data }) => {
   const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
+  const[loading , setLoading] = useState(false);
+  const[filteredData , setFilteredData] = useState([]);
 
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(() => {
+      setFilteredData(data);
+      setLoading(false);
+    }, 1000);
+
+  },[currentLanguage , data]);
+
+  if(loading){
+    return (
+      <div className='data-component'>
+            <Oval
+              height={80}
+              width={80}
+              color="#4fa94d"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              ariaLabel="oval-loading"
+              secondaryColor="#4fa94d"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+      </div>
+    )
+  }
 
   return (
     <div className="data-component">
-      {data.map((item) => (
+      {filteredData.map((item) => (
         <div key={item.id} className="data-item">
           <h3>{currentLanguage === 'en' ? item.nameen : item.nametr}</h3>
           <p>{currentLanguage === 'en' ? item.descen : item.desctr}</p>
